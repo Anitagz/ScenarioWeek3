7eb9cf3a987f071557f576113569bfaab0b7f57b
dialadahabi <dahabidiala@hotmail.com>
2016-01-12 16:06:29 +0000

    lamp class

---
+++ circuitSimulator/src/circuitsimulator/lamp.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package circuitsimulator;
+
+/**
+ *
+ * @author dialadahabi1
+ */
+public class lamp {
+    int on = 1;
+    int off = 0;
+    private resistance setResistance;
+    
+public int getStatus(){
+    return 0;
+}
+
+
+public void setResistance(){
+}
+
+    
+    
+}
