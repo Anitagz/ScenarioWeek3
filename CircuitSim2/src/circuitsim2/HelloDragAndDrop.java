d0bd6c5b6de2621cbdd26eae56ed3d9184e51e0f
stupidjc <stupidjc@Johnsons-MacBook-Pro.local>
2016-01-13 17:20:40 +0000

    done

--- CircuitSim2/src/circuitsim2/HelloDragAndDrop.java
+++ CircuitSim2/src/circuitsim2/HelloDragAndDrop.java
@@ -8,20 +8,16 @@ package circuitsim2;
 
 import javafx.application.Application;
 import javafx.event.EventHandler;
-import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.*;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -50,58 +46,54 @@ public class HelloDragAndDrop extends Application {
         menuBar.getMenus().addAll(menuFile, menuEdit, menuHelp);
         grid.add(menuBar, 0, 0, 2, 1);
         
+        int symbolSize = 50;
+        
         Image img1 = new Image(getClass().getResourceAsStream("ammeter.png"));
         ImageView ammeterSym = new ImageView(img1);
-        ammeterSym.setFitHeight(40);
-        ammeterSym.setFitWidth(40);
+        ammeterSym.setFitHeight(symbolSize);
+        ammeterSym.setFitWidth(symbolSize);
         
         Image img2 = new Image(getClass().getResourceAsStream("voltmeter.png"));
         ImageView voltmeterSym = new ImageView(img2);
-        voltmeterSym.setFitHeight(40);
-        voltmeterSym.setFitWidth(40);
+        voltmeterSym.setFitHeight(symbolSize);
+        voltmeterSym.setFitWidth(symbolSize);
         
         Image img3 = new Image(getClass().getResourceAsStream("battery.png"));
         ImageView batterySym = new ImageView(img3);
-        batterySym.setFitHeight(40);
-        batterySym.setFitWidth(40);
+        batterySym.setFitHeight(symbolSize);
+        batterySym.setFitWidth(symbolSize);
         
         Image img4 = new Image(getClass().getResourceAsStream("lamp.png"));
         ImageView lampSym = new ImageView(img4);
-        lampSym.setFitHeight(40);
-        lampSym.setFitWidth(40);
+        lampSym.setFitHeight(symbolSize);
+        lampSym.setFitWidth(symbolSize);
         
         Image img5 = new Image(getClass().getResourceAsStream("resistor.png"));
         ImageView resistorSym = new ImageView(img5);
-        resistorSym.setFitHeight(40);
-        resistorSym.setFitWidth(40);
+        resistorSym.setFitHeight(symbolSize);
+        resistorSym.setFitWidth(symbolSize);
         
         Image img6 = new Image(getClass().getResourceAsStream("openswitch.png"));
         ImageView openswitchSym = new ImageView(img6);
-        openswitchSym.setFitHeight(40);
-        openswitchSym.setFitWidth(40);
+        openswitchSym.setFitHeight(symbolSize);
+        openswitchSym.setFitWidth(symbolSize);
+        
+        setupGestureSource(batterySym);
+        setupGestureSource(lampSym);
+        setupGestureSource(resistorSym);
+        setupGestureSource(ammeterSym);
+        setupGestureSource(voltmeterSym);
+        
         
         GridPane componentsGrid = new GridPane();
         componentsGrid.add(batterySym,0,0);
-        componentsGrid.add(lampSym,0,1);
-        //componentsGrid.add(resistorSym,1,0);
-        //componentsGrid.add(openswitchSym,1,1);
-        //componentsGrid.add(ammeterSym, 2, 0);
-        //componentsGrid.add(voltmeterSym,2,1);
+        componentsGrid.add(lampSym,1,0);
+        componentsGrid.add(resistorSym,0,1);
+        componentsGrid.add(openswitchSym,1,1);
+        componentsGrid.add(ammeterSym, 0, 2);
+        componentsGrid.add(voltmeterSym,1,2);
         
-        
-        
-        
-       // VBox vbox = new VBox(0);
-        //vbox.setAlignment(Pos.CENTER);
-        
-       /* vbox.getChildren().addAll(
-                ammeterSym,
-                voltmeterSym
-                
-        );*/
-        
-        //grid.add(componentsGrid,0,1);
-        
+        grid.add(componentsGrid,0,1);
         
         
         GridPane canvasGrid = new GridPane();
@@ -113,23 +105,23 @@ public class HelloDragAndDrop extends Application {
             for(int i=0; i < 15; i++){
                 //canvasGrid.add(gridBG,i,0);
 
-                StackPane stpane = new StackPane();
+                Pane stpane = new StackPane();
                 stpane.setMinSize(40, 40);
                 stpane.setMaxSize(40,40);
                 stpane.setPrefSize(40, 40);
+                setupGestureTarget(stpane);
                 canvasGrid.add(stpane,i,j);
             }
         }
         
 
-        final Text source = new Text(50, 100, "DRAG ME");
-        source.setScaleX(2.0);
-        source.setScaleY(2.0);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
 
-        final Text target = new Text(250, 100, "DROP HERE");
-        target.setScaleX(2.0);
-        target.setScaleY(2.0);
-
+    void setupGestureSource(ImageView source){
+        
         source.setOnDragDetected(new EventHandler <MouseEvent>() {
             public void handle(MouseEvent event) {
                 /* drag was detected, start drag-and-drop gesture*/
@@ -140,14 +132,29 @@ public class HelloDragAndDrop extends Application {
                 
                 /* put a string on dragboard */
                 ClipboardContent content = new ClipboardContent();
-                content.putString(source.getText());
+                content.putString("hi");
                 db.setContent(content);
                 
                 event.consume();
             }
         });
-
-        target.setOnDragOver(new EventHandler <DragEvent>() {
+        
+        source.setOnDragDone(new EventHandler <DragEvent>() {
+            public void handle(DragEvent event) {
+                /* the drag-and-drop gesture ended */
+                System.out.println("onDragDone");
+                /* if the data was successfully moved, clear it */
+                if (event.getTransferMode() == TransferMode.MOVE) {
+                 //   source.setText("");
+                }
+                
+                event.consume();
+            }
+        });
+    }
+    
+    void setupGestureTarget(Pane target){
+         target.setOnDragOver(new EventHandler <DragEvent>() {
             public void handle(DragEvent event) {
                 /* data is dragged over the target */
                 System.out.println("onDragOver");
@@ -160,6 +167,9 @@ public class HelloDragAndDrop extends Application {
                     event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
                 }
                 
+                
+                    target.setStyle("-fx-background-color:blue;");
+                
                 event.consume();
             }
         });
@@ -171,8 +181,12 @@ public class HelloDragAndDrop extends Application {
                 /* show to the user that it is an actual gesture target */
                 if (event.getGestureSource() != target &&
                         event.getDragboard().hasString()) {
-                    target.setFill(Color.GREEN);
+                    //target.setFill(Color.GREEN);
+                    
                 }
+                
+                
+                    target.setStyle("-fx-background-color:blue;");
                 
                 event.consume();
             }
@@ -181,8 +195,9 @@ public class HelloDragAndDrop extends Application {
         target.setOnDragExited(new EventHandler <DragEvent>() {
             public void handle(DragEvent event) {
                 /* mouse moved away, remove the graphical cues */
-                target.setFill(Color.BLACK);
+                //target.setFill(Color.BLACK);
                 
+                target.setStyle("-fx-background-color:transparent;");
                 event.consume();
             }
         });
@@ -195,7 +210,7 @@ public class HelloDragAndDrop extends Application {
                 Dragboard db = event.getDragboard();
                 boolean success = false;
                 if (db.hasString()) {
-                    target.setText(db.getString());
+                    //target.setText(db.getString());
                     success = true;
                 }
                 /* let the source know whether the string was successfully 
@@ -205,26 +220,8 @@ public class HelloDragAndDrop extends Application {
                 event.consume();
             }
         });
-
-        source.setOnDragDone(new EventHandler <DragEvent>() {
-            public void handle(DragEvent event) {
-                /* the drag-and-drop gesture ended */
-                System.out.println("onDragDone");
-                /* if the data was successfully moved, clear it */
-                if (event.getTransferMode() == TransferMode.MOVE) {
-                    source.setText("");
-                }
-                
-                event.consume();
-            }
-        });
-
-        //grid.getChildren().add(source);
-        //grid.getChildren().add(target);
-        stage.setScene(scene);
-        stage.show();
     }
-
+    
     public static void main(String[] args) {
         Application.launch(args);
     }
