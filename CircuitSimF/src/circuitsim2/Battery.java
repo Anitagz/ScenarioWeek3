33c19dee0a4814ff32b14018f066afcb64f20695
Anitagz <anita.gzadeh@gmail.com>
2016-01-14 14:05:31 +0000

    Sorry :(

--- CircuitSimF/src/circuitsim2/Battery.java
+++ CircuitSimF/src/circuitsim2/Battery.java
@@ -1,23 +1,29 @@
-package circuitsim2;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package circuitsim2;
 
 /**
  *
  * @author Anita
  */
-
-/*
 public class Battery extends Component {
     
-    public Battery(String name){
-        super(name);
+    //private String name;
+    private double voltage;
+  
+    public Battery(String n){
+        super(n);
+        voltage = 9;
     }
     
+    public void setVoltage(double v){
+        this.voltage = v;
+    }
+    
+    public double getVoltage(){
+        return voltage;
+    }   
 }
-
-*/
