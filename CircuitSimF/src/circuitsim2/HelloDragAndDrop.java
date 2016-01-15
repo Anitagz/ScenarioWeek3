2a27e3e7f34bdafd7a9198dff54de1cc0914991e
stupidjc <stupidjc@dhcp-59-66.xlate.eduroam.ucl.ac.uk>
2016-01-15 11:02:15 +0000

    j

--- CircuitSimF/src/circuitsim2/HelloDragAndDrop.java
+++ CircuitSimF/src/circuitsim2/HelloDragAndDrop.java
@@ -317,12 +317,8 @@ public class HelloDragAndDrop extends Application {
                            else if(circuit.getComponent(i, j) instanceof Switch){
                                 Image img1 = new Image(getClass().getResourceAsStream("Images/openswitch.png"));
                                 iv1.setImage(img1);
-<<<<<<< HEAD
-                                setupGestureSource(iv1, "switch");
                                 System.out.println("s");
-=======
                                 setupGestureSource(iv1, "openswitch");
->>>>>>> origin/master
                                
                            }
                            else if(circuit.getComponent(i, j) instanceof Wire){
